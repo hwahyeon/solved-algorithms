c149a27bd7b07b4fde26150177bf79c6713f93c8
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2021-08-10 20:45:12 +0900

    Create Codewars | You Can't Code Under Pressure #1.lisp

---
+++ CommonLisp/Codewars | You Can't Code Under Pressure #1.lisp
@@ -0,0 +1,8 @@
+(defpackage #:challenge/solution
+  (:use #:cl)
+  (:export #:double-integer))
+(in-package #:challenge/solution)
+
+(defun double-integer (n)
+  ( * 2 n); n will be an integer. Double it and return it.
+  )

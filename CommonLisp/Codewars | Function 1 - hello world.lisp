19289dc6f8786488c0c57c858b0f53bfb465daa3
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2021-08-10 21:02:06 +0900

    Create Codewars | Function 1 - hello world.lisp

---
+++ CommonLisp/Codewars | Function 1 - hello world.lisp
@@ -0,0 +1,9 @@
+(defpackage #:challenge/solution
+  (:use #:cl)
+  (:export #:greet))
+(in-package #:challenge/solution)
+
+; Write a function `greet` that returns "hello world!"
+
+(defun greet()
+  "hello world!")

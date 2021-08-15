018df3c55cbfc96963c92f85b343d0e77f1c931a
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2021-08-16 00:31:14 +0900

    Create Codewars | Drying Potatoes.lisp

---
+++ CommonLisp/Codewars | Drying Potatoes.lisp
@@ -0,0 +1,8 @@
+(defpackage #:challenge/solution
+  (:use #:cl)
+  (:export #:potatoes))
+(in-package #:challenge/solution)
+
+(defun potatoes (p0 w0 p1)
+  (truncate (* w0 ( / (- 100 p0) (- 100 p1))))
+)

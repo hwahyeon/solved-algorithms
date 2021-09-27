f6d311cd81612c696e6bfc59da31c07796ae4d79
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2021-09-27 23:21:38 +0900

    Create BaekJoon | 약수들의 합.py

---
+++ Python/BaekJoon | 약수들의 합.py
@@ -0,0 +1,12 @@
+while 1:
+    n = int(input())
+    if n == -1:
+        break
+    arr = []
+    for i in range(1, n):
+        if n % i == 0:
+            arr.append(i)
+    if sum(arr) == n:
+        print(n, " = ", " + ".join(map(str, arr)), sep="")
+    else:
+        print(n, "is NOT perfect.")

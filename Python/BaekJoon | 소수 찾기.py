b83072c0d113bb7ac4e9ed231bb53c6f281eb65a
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2021-10-08 23:36:39 +0900

    Create BaekJoon | 소수 찾기.py

---
+++ Python/BaekJoon | 소수 찾기.py
@@ -0,0 +1,13 @@
+n = input()
+lst = list(map(int, input().split()))
+cnt = 0
+
+for num in lst:
+    error = 0
+    if num > 1 :
+        for i in range(2, num):
+            if num % i == 0:
+                error += 1
+        if error == 0:
+            cnt += 1
+print(cnt)

4a61a76e53715c52f3d66b8108c3e4fffd4def01
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2021-10-23 17:22:37 +0900

    Create Codewars | SQL with Sailor Moon: Thinking about JOINs....sql

---
+++ SQL/Codewars | SQL with Sailor Moon: Thinking about JOINs....sql
@@ -0,0 +1,8 @@
+SELECT
+  senshi_name as sailor_senshi,
+  real_name_jpn as real_name,
+  cats.name as cat,
+  school
+FROM sailorsenshi
+  LEFT JOIN schools ON sailorsenshi.school_id=schools.id
+  LEFT JOIN cats ON sailorsenshi.cat_id=cats.id

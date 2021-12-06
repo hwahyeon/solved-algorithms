90d95d181173826b3e5d05af09e068532824b2f9
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2021-12-06 23:05:19 +0900

    Create Codewars | Drink about.py

---
+++ Python/Codewars | Drink about.py
@@ -0,0 +1,9 @@
+def people_with_age_drink(age):
+    if age >= 21:
+        return "drink whisky"
+    elif age >= 18:
+        return "drink beer"
+    elif age >= 14:
+        return "drink coke"
+    else:
+        return "drink toddy"

879fa7f815df4ec194f7b4a8bb145438e3aa5cfc
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2021-12-16 16:24:42 +0900

    Create Codewars | 101 Dalmatians - squash the bugs, not the dogs!.r

---
+++ R/Codewars | 101 Dalmatians - squash the bugs, not the dogs!.r
@@ -0,0 +1,12 @@
+#Fix me!
+
+how_many_dalmatians <- function( n ) {
+    if (n == 101) {
+      return("101 DALMATIANS!!!")
+    } else if (n < 11) {
+      return("Hardly any")
+    } else if (n < 51) {
+      return("More than a handful!")
+    } else {
+    return("Woah that's a lot of dogs!")}
+}

bcb991dbf35c5c0767fb296014ba9114c7f42216
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2021-12-17 21:35:37 +0900

    Create Codewars | Basic Mathematical Operations.r

---
+++ R/Codewars | Basic Mathematical Operations.r
@@ -0,0 +1,6 @@
+basic_op <- function(operator, value1, value2){
+  if(operator == '+'){value1 + value2}
+  else if(operator == '-'){value1 - value2}
+  else if(operator == '*'){value1 * value2}
+  else if(operator == '/'){value1 / value2}
+}

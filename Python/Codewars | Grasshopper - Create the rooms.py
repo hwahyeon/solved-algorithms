5b1f4c35e05e69130d23bb024671e7178135de01
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2022-02-02 23:41:32 +0900

    Create Codewars | Grasshopper - Create the rooms.py

---
+++ Python/Codewars | Grasshopper - Create the rooms.py
@@ -0,0 +1,11 @@
+rooms = {
+    'room_1':{'name': 'a',
+           'description':'a',
+           'completed': 'a'},
+    'room_2':{'name': 'a',
+           'description':'a',
+           'completed': 'a'},
+    'room_3':{'name': 'a',
+           'description':'a',
+           'completed': 'a'}
+}

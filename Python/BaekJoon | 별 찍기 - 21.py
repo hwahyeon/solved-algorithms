419495663f089f054f34a5fdc75f1db92c666062
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2022-02-18 23:24:48 +0900

    Update BaekJoon | 별 찍기 - 21.py

--- Python/BaekJoon | 별 찍기 - 21.py
+++ Python/BaekJoon | 별 찍기 - 21.py
@@ -1,12 +1,12 @@
 n = int(input())
+
 if n == 1:
     print('*')
 elif n % 2 == 0:
-    for i in range(int(n/2)):
-        print('* ' * n)
-        print(' *' * n)
+    for i in range(n):
+        print('* ' * int(n/2))
+        print(' *' * int(n/2))
 else:
-    for i in range(int(n/2)):
-        print('* ' * n)
-        print(' *' * n)
-    print('* ' * n)
+    for i in range(n):
+        print('* ' * (int(n/2)+1))
+        print(' *' * int(n/2))

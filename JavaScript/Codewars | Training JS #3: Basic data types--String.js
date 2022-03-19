23cfa9f4104ca4b829b8c46a713560e459a688f1
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2022-03-19 21:02:40 +0900

    Create Codewars | Training JS #3: Basic data types--String.js

---
+++ JavaScript/Codewars | Training JS #3: Basic data types--String.js
@@ -0,0 +1,53 @@
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad(){
+  //select some variable to combine "Dad"
+  return d1+a2+d2;
+}
+function Bee(){
+  //select some variable to combine "Bee"
+  return b1+e2+e2;
+}
+function banana(){
+  //select some variable to combine "banana"
+  return b2+a2+n2+a2+n2+a2;
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+
+
+
+*/
+answers
+
+1
+John's iron axe fell into the river. He cried sadly by the river.
+Then a monster came out of the water, with a golden axe in his hand.
+He asked John: is this golden axe yours?
+The monster frowned and said to John: "This is not your axe,
+you are not an honest boy!!!"
+
+2
+John replied that it was not his axe. The monster took out a silver axe.
+He asked John: is this silver axe yours?
+The monster frowned and said to John: "This is not your axe,
+you are not an honest boy!!!"
+
+3
+John replied that it was not his axe. The monster took out a iron axe.
+He asked John: is this iron axe yours?
+The monster frowned and said to John: "This is your axe,
+are you a foolish boy???"
+
+*/

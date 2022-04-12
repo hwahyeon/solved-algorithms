0e7e1e6eb5387839b58b53c42d06ec7ee5ed1a32
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2022-04-12 23:16:36 +0900

    Update BaekJoon | 단어 정렬.py

--- Python/BaekJoon | 단어 정렬.py
+++ Python/BaekJoon | 단어 정렬.py
@@ -1,3 +1,24 @@
+# 시간 초과
+import sys
+
+def min_word_finder(res):
+    min_word = None
+    for word in res:
+        if min_word == None or len(word) < len(min_word):
+            min_word = word
+    return min_word
+
+res = [sys.stdin.readline().strip() for i in range(int(sys.stdin.readline()))]
+res = sorted(list(set(res)))
+
+for i in range(len(res)):
+    n = res.index(min_word_finder(res[i:]))
+    res[i], res[n] = res[n], res[i]
+
+for j in res:
+    print(j)
+
+# re
 import sys
 
 res = [sys.stdin.readline().strip() for i in range(int(sys.stdin.readline()))]

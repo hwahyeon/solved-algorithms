105bad39b373e6475accfdcbecf5d59d31d9f96d
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2022-04-24 17:02:20 +0900

    Add new solution

--- Python/BaekJoon | 애너그램 만들기.py
+++ Python/BaekJoon | 애너그램 만들기.py
@@ -6,3 +6,11 @@ for j in list(input()):
     lst[ord(j) - 97] -= 1
 
 print(sum(map(abs, lst)))
+
+# Add new solution
+a = list(input())
+b = list(input())
+a1, b1 = a[:], b[:]
+out1 = [x for x in a if not x in b1 or b1.remove(x)]
+out2 = [x for x in b if not x in a1 or a1.remove(x)]
+print(len(out1)+len(out2))

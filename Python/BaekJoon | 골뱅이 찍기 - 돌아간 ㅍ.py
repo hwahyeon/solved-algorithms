d0f5f4c7c67b527836fbd167434de182fcdae914
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2022-06-11 22:28:03 +0900

    Create BaekJoon | 골뱅이 찍기 - 돌아간 ㅍ.py

---
+++ Python/BaekJoon | 골뱅이 찍기 - 돌아간 ㅍ.py
@@ -0,0 +1,11 @@
+n = int(input())
+for i in range(n):
+    print('@'*n + ' '*(3*n) + '@'*n)
+for j in range(n):
+    print('@'*(n*5))
+for k in range(n):
+    print('@'*n + ' '*(3*n) + '@'*n)
+for l in range(n):
+    print('@'*(n*5))
+for m in range(n):
+    print('@'*n + ' '*(3*n) + '@'*n)

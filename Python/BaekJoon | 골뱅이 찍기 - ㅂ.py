121d1c5295be34714ec638b25a94935845c7e836
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2022-06-11 22:27:30 +0900

    Create BaekJoon | 골뱅이 찍기 - ㅂ.py

---
+++ Python/BaekJoon | 골뱅이 찍기 - ㅂ.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(2*n):
+    print('@'*n + ' '*(3*n) + '@'*n)
+for j in range(n):
+    print('@'*(n*5))
+for k in range(n):
+    print('@'*n + ' '*(3*n) + '@'*n)
+for l in range(n):
+    print('@'*(n*5))

3fb5896280fdedf95f500dd5138f2eb5230674e2
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2022-07-08 23:43:16 +0900

    Create Codewars | Fuel Calculator.js

---
+++ JavaScript/Codewars | Fuel Calculator.js
@@ -0,0 +1,7 @@
+function fuelPrice(litres, pricePerLitre) {
+  for(let i = 2; i <= 10; i += 2){
+    if (litres >= i){
+      pricePerLitre -= 0.05
+    }
+  } return +(litres * pricePerLitre).toFixed(2)
+}

043c7b4c52d6912dbdc295e74ae9e0870135de71
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2022-07-11 00:03:38 +0900

    Update BaekJoon | Do Not Touch Anything.py

--- Python/BaekJoon | Do Not Touch Anything.py
+++ Python/BaekJoon | Do Not Touch Anything.py
@@ -1,4 +1,4 @@
 R, C, N = map(int, input().split())
-a = R//N + 1 if R%N else R//N
-b = C//N + 1 if C%N else C//N
-print(a * b)
+A = R//N + 1 if R % N else R//N
+B = C//N + 1 if C % N else C//N
+print(A * B)

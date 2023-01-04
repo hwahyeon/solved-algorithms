ad16a455771ad9e257902b1af663635b46fefcba
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2023-01-04 11:38:30 +0900

    Create BaekJoon | Livestock Count.adb

---
+++ Ada/BaekJoon | Livestock Count.adb
@@ -0,0 +1,12 @@
+with Text_IO;
+procedure Livestock_Count is
+
+begin
+  Text_IO.Put_Line("Animal      Count");
+  Text_IO.Put_Line("-----------------");
+  Text_IO.Put_Line("Chickens      100");
+  Text_IO.Put_Line("Clydesdales     5");
+  Text_IO.Put_Line("Cows           40");
+  Text_IO.Put_Line("Goats          22");
+  Text_IO.Put_Line("Steers          2");
+end Livestock_Count;

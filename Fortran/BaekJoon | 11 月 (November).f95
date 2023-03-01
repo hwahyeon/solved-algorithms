baf6c2b48d4ea447f6388eb3bc45bcc735a2b08f
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2023-03-01 22:57:24 +0900

    Update and rename 11 月 (November).f95 to BaekJoon | 11 月 (November).f95

--- Fortran/BaekJoon | 11 月 (November).f95
+++ Fortran/BaekJoon | 11 月 (November).f95
@@ -1,21 +1,11 @@
 PROGRAM P27332
-
     INTEGER A, B, R
-
     READ(*,*) A
-
     READ(*,*) B
-
     R = B * 7 + A
-
     if (R .LE. 30) then
-
         WRITE(*, '(I0)') 1
-
     else
-
         WRITE(*, '(I0)') 0
-
     end if
-
 END PROGRAM P27332

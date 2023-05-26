0827fbc8462a9bda88983dde4bee0bcce8e7aa5b
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2023-05-26 23:20:08 +0900

    Create BaekJoon | 問題 １.py

---
+++ Python/BaekJoon 1/BaekJoon | 問題 １.py
@@ -0,0 +1,14 @@
+res1, res2 = 0, 0
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+
+    if a > b:
+        res1 += (a + b)
+    elif a < b:
+        res2 += (a + b)
+    else:
+        res1 += a
+        res2 += b
+
+print(res1, res2)

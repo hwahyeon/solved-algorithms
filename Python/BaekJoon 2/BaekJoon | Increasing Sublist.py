db4c85269b346a29a3add4a6e6dd45dc5e44bb65
hwahyeon <46473089+hwahyeon@users.noreply.github.com>
2023-09-28 23:50:26 +0900

    Create BaekJoon | Increasing Sublist.py

---
+++ Python/BaekJoon 2/BaekJoon | Increasing Sublist.py
@@ -0,0 +1,12 @@
+n = int(input().strip())
+arr = list(map(int, input().split()))
+max_len = 1
+current_len = 1
+
+for i in range(1, n):
+    if arr[i] > arr[i-1]:
+        current_len += 1
+        max_len = max(max_len, current_len)
+    else:
+        current_len = 1
+print(max_len)
